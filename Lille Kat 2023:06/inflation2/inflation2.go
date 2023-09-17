8041f0db553d92e2115cc2ff862a1a7e4452a57c
Andreas Wachs <andreas@wachs.dk>
2023-09-18 00:09:02 +0200

    100/100, 0.14s

--- Lille Kat 2023:06/inflation2/inflation2.go
+++ Lille Kat 2023:06/inflation2/inflation2.go
@@ -1,3 +1,5 @@
+// Package provides a 100/100 soluton to the inflation2 problem on Kattis
+// with a runtime of 0.14 seconds
 package main
 
 import (
@@ -8,125 +10,20 @@ import (
 	"strings"
 )
 
-// 28/88 accepted cases, this is too slow
-
 var reader = bufio.NewReader(os.Stdin)
 var ws = bufio.NewWriter(os.Stdout)
+
 var globalInflation int64 = 0
-
-type node struct {
-	price  int64
-	amount int64
-	left   *node
-	right  *node
-}
-
-type BinaryTree struct {
-	root *node
-}
-
-func NewBinaryTree() *BinaryTree {
-	return &BinaryTree{}
-}
-
-func (t *BinaryTree) Insert(price int64, amount int64) {
-	newNode := &node{
-		price:  price,
-		amount: amount,
-	}
-
-	if t.root == nil {
-		t.root = newNode
-	} else {
-		t.insertNode(t.root, newNode)
-	}
-}
-
-func (t *BinaryTree) Set(x, y int64) {
-	t.setNode(t.root, x, y)
-}
-
-func (t *BinaryTree) setNode(head *node, x, y int64) {
-	if head == nil {
-		return
-	}
-
-	// Recursively traverse the tree
-	// Check if the current node's value is x
-	if head.price == x {
-		// Transfer the amount to the node with value y
-		// ws.WriteString("setNode found x\n")
-		t.Insert(y, head.amount)
-		head.amount = 0
-		return
-	}
-
-	t.setNode(head.left, x, y)
-	t.setNode(head.right, x, y)
-}
-
-func (t *BinaryTree) insertNode(root *node, newNode *node) {
-	if newNode.price < root.price {
-		if root.left == nil {
-			root.left = newNode
-		} else {
-			t.insertNode(root.left, newNode)
-		}
-	} else if newNode.price > root.price {
-		if root.right == nil {
-			root.right = newNode
-		} else {
-			t.insertNode(root.right, newNode)
-		}
-	} else {
-		// If a node with the same price already exists, update its amount
-		// ws.WriteString(fmt.Sprintf("Updating node with price %d. Amount before: %d, amount after: %d. Amount added: %d\n", newNode.price, root.amount, root.amount+newNode.amount, newNode.amount))
-		root.amount += newNode.amount
-	}
-}
-
-func (t *BinaryTree) Print() {
-	t.printNode(t.root)
-	ws.WriteString("\n")
-}
-
-func (t *BinaryTree) printNode(root *node) {
-	if root == nil {
-		return
-	}
-
-	t.printNode(root.left)
-
-	// Print the node's price and amount
-	// ws.WriteString("(Price: " + strconv.FormatInt(globalInflation+root.price, 10) + ", Amount: " + strconv.FormatInt(root.amount, 10) + ")")
-
-	t.printNode(root.right)
-}
-
-func (t *BinaryTree) Sum(globalInflation int64) int64 {
-	return t.sumNode(t.root, globalInflation)
-}
-
-func (t *BinaryTree) sumNode(root *node, globalInflation int64) int64 {
-	if root == nil {
-		return 0
-	}
-
-	sum := (root.price + globalInflation) * root.amount
-	sum += t.sumNode(root.left, globalInflation)
-	sum += t.sumNode(root.right, globalInflation)
-
-	return sum
-}
+var table = make(map[int64]int64)
+var sum int64 = 0
+var totalAmount int64 = 0
 
 func main() {
+	totalAmount = readInt64()
 
-	tree := NewBinaryTree()
-
-	n := readInt()
-	for i := 0; i < n; i++ {
-		price := int64(readInt())
-		tree.Insert(price, 1)
+	for _, price := range readArrInt64() {
+		table[price]++
+		sum += price
 	}
 
 	days := readInt()
@@ -136,24 +33,30 @@ func main() {
 	for i := 0; i < days; i++ {
 		line := readLine()
 
-		//tree.Print()
-		// ws.WriteString(fmt.Sprint(line, "\n"))
-
 		if line[0] == 'I' {
-			fmt.Sscanf(line, "INFLATION %d", &inflation)
+			inflation = readInt64From(line[10:])
 
 			globalInflation += inflation
+			sum += totalAmount * inflation
 		} else {
-			fmt.Sscanf(line, "SET %d %d", &x, &y)
+			tokens := strings.Split(line, " ")
+			x = readInt64From(tokens[1])
+			y = readInt64From(tokens[2])
 
 			if x != y {
-				tree.Set(x-globalInflation, y-globalInflation)
+				if _, ok := table[x-globalInflation]; !ok {
+					goto printSum
+				}
+
+				table[y-globalInflation] += table[x-globalInflation]
+
+				sum += (y - x) * table[x-globalInflation]
+				delete(table, x-globalInflation)
+
 			}
 		}
-
-		// tree.Print()
-
-		ws.WriteString(fmt.Sprint(tree.Sum(globalInflation)) + "\n")
+	printSum:
+		ws.WriteString(fmt.Sprint(sum) + "\n")
 	}
 
 	ws.Flush()
@@ -168,25 +71,86 @@ func readLine() string {
 	return line[:len(line)-1]
 }
 
-// readInt reads a single int from stdin
 func readInt() int {
-	var x int
-	fmt.Scan(&x)
-	return x
-}
-
-// readInts reads a line of space-separated ints from stdin
-func readInts() []int {
-	line := readLine()
-	var xs []int
-
-	for _, num := range strings.Split(line, " ") {
-		n, err := strconv.Atoi(num)
-		if err != nil {
-			panic(err)
-		}
-		xs = append(xs, n)
+	nStr, e := reader.ReadString('\n')
+	if e != nil {
+		panic(e)
 	}
 
-	return xs
+	nStr = nStr[:len(nStr)-1]
+	n, e := strconv.Atoi(nStr)
+	if e != nil {
+		panic(e)
+	}
+
+	return n
+}
+
+func readInt64() int64 {
+	nStr, e := reader.ReadString('\n')
+	if e != nil {
+		panic(e)
+	}
+	nStr = nStr[:len(nStr)-1]
+	n, e := strconv.ParseInt(nStr, 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return n
+}
+
+func readInt64From(line string) int64 {
+	n, e := strconv.ParseInt(line, 10, 64)
+	if e != nil {
+		panic(e)
+	}
+	return n
+}
+
+func readLineNumbs() []string {
+	line, e := reader.ReadString('\n')
+	if e != nil {
+		panic(e)
+	}
+	line = line[:len(line)-1]
+	numbs := strings.Split(line, " ")
+	return numbs
+}
+
+func readArrInt() []int {
+	numbs := readLineNumbs()
+	arr := make([]int, len(numbs))
+	for i, n := range numbs {
+		val, e := strconv.Atoi(n)
+		if e != nil {
+			panic(e)
+		}
+		arr[i] = val
+	}
+	return arr
+}
+
+func readArrInt64() []int64 {
+	numbs := readLineNumbs()
+	arr := make([]int64, len(numbs))
+	for i, n := range numbs {
+		val, e := strconv.ParseInt(n, 10, 64)
+		if e != nil {
+			panic(e)
+		}
+		arr[i] = val
+	}
+	return arr
+}
+
+func readArrInt64From(numbs []string, start int) []int64 {
+	arr := make([]int64, len(numbs)-start)
+	for i, n := range numbs[start:] {
+		val, e := strconv.ParseInt(n, 10, 64)
+		if e != nil {
+			panic(e)
+		}
+		arr[i] = val
+	}
+	return arr
 }
